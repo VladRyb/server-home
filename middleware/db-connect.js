0b39cada79699b18bbaa0a5d17f39b9fdc91b70f
Vlad Ryb <ribakov@corp.sputnik.ru>
2021-04-26 14:25:20 +0300

    DB

--- middleware/db-connect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/home", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-export default mongoose.connection;
--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,16 @@
 import { urlencoded, json } from "express";
 import morgan from "morgan";
-import dbConnection from "./db-connect.js";
+// import dbConnection from "./db-connect.js";
+import mongoose from "mongoose";
 
 export default function (app) {
+  mongoose.connect(
+    `mongodb+srv://admin:${process.env.PASSWORD}@cluster0.qovlf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
   app.use(morgan("dev"));
 
   // Body POST запросов.
